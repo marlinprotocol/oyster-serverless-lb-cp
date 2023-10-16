5e67d2402035b54d06875b190e731121b2078c6b
arnav-deep <arnavd4@gmail.com>
2023-10-16 16:09:07 +0530

    remove server

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod nginx_utils;
 
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-use nginx_utils::ServerInfo;
+use nginx_utils::{AddServerInfo, RemoveServerInfo};
 
 #[get("/")]
 async fn index() -> impl Responder {
@@ -14,18 +14,24 @@ async fn soft_reload() -> impl Responder {
     let res = nginx_utils::soft_reload_nginx().await;
 
     if !res.is_err() {
-        return HttpResponse::InternalServerError().body("Failed to soft reload nginx");
+        return HttpResponse::InternalServerError().body(format!(
+            "Failed to soft reload nginx. Error :{:?}",
+            res.err().unwrap()
+        ));
     }
 
     HttpResponse::Ok().body("Soft reload succesful")
 }
 
 #[post("/add-server")]
-async fn add_server(web::Json(server): web::Json<ServerInfo>) -> impl Responder {
+async fn add_server(web::Json(server): web::Json<AddServerInfo>) -> impl Responder {
     let res = nginx_utils::add_server(server).await;
 
     if res.is_err() {
-        return HttpResponse::InternalServerError().body("Failed to add server");
+        return HttpResponse::InternalServerError().body(format!(
+            "Failed to add server. Error :{:?}",
+            res.err().unwrap()
+        ));
     }
 
     let res = res.unwrap();
@@ -36,6 +42,26 @@ async fn add_server(web::Json(server): web::Json<ServerInfo>) -> impl Responder 
     ))
 }
 
+#[post("/remove-server")]
+async fn remove_server(web::Json(server): web::Json<RemoveServerInfo>) -> impl Responder {
+    let res = nginx_utils::remove_server(server.ip).await;
+
+    if res.is_err() {
+        return HttpResponse::InternalServerError().body(format!(
+            "Failed to remove server. Error :{:?}",
+            res.err().unwrap()
+        ));
+    }
+
+    let res = res.unwrap();
+
+    if !res {
+        return HttpResponse::BadRequest().body("Server with the IP is not registered");
+    }
+
+    HttpResponse::Ok().body("Server removed successfully")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
@@ -43,6 +69,7 @@ async fn main() -> std::io::Result<()> {
             .service(index)
             .service(soft_reload)
             .service(add_server)
+            .service(remove_server)
     })
     .bind(("000000000", 8012))?
     .run()
--- src/nginx_utils.rs
+++ src/nginx_utils.rs
@@ -30,12 +30,12 @@ pub async fn soft_reload_nginx() -> Result<()> {
 }
 
 #[derive(Deserialize)]
-pub struct ServerInfo {
+pub struct AddServerInfo {
     ip: String,
     capacity: u64,
 }
 
-pub async fn add_server(server: ServerInfo) -> Result<(String, u64, u64)> {
+pub async fn add_server(server: AddServerInfo) -> Result<(String, u64, u64)> {
     let ip = server.ip;
     let weight = server.capacity - ENCLAVE_IMAGE_INITIAL_USED_CAPACITY_MB;
     let max_conns = weight / ALLOTMENT_PER_WORKERD_MB;
@@ -70,11 +70,22 @@ pub async fn add_server(server: ServerInfo) -> Result<(String, u64, u64)> {
     let mut lines = reader.lines();
     let mut contents = String::new();
 
+    let mut added: bool = false;
     while let Some(line) = lines.next() {
         let line = line.unwrap();
-        contents.push_str(&line);
-        if line.contains("# servers") {
-            contents.push_str(format!("{line_to_add}").as_str());
+
+        if !added {
+            if line.contains(format!("server {}", &ip).as_str()) {
+                contents.push_str(format!("{line_to_add}").as_str());
+                added = true;
+            }
+            if line.contains("# SERVERS -- END") {
+                contents.push_str(format!("{line_to_add}\n").as_str());
+                contents.push_str(&line);
+                added = true;
+            }
+        } else {
+            contents.push_str(&line);
         }
         contents.push_str("\n");
     }
@@ -100,4 +111,73 @@ pub async fn add_server(server: ServerInfo) -> Result<(String, u64, u64)> {
     let _ = filelock.unlock();
 
     Ok((ip, weight, max_conns))
+}
+
+#[derive(Deserialize)]
+pub struct RemoveServerInfo {
+    pub ip: String,
+}
+
+pub async fn remove_server(ip: String) -> Result<bool> {
+    let options = FileOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true);
+
+    let filelock = match FileLock::lock(NGINX_CONF_PATH, true, options) {
+        Ok(lock) => lock,
+        Err(err) => panic!("Error getting write lock: {:#?}", err),
+    };
+
+    let output = Command::new("cp")
+        .arg(format!("{NGINX_CONF_PATH}"))
+        .arg(format!("{NGINX_CONF_BACKUP_PATH}"))
+        .output()
+        .expect("Failed to create backup file");
+
+    if !output.status.success() {
+        return Err(anyhow::anyhow!(
+            "Failed to create backup file: {}",
+            String::from_utf8(output.stderr).unwrap()
+        ));
+    }
+
+    let file = File::open(NGINX_CONF_PATH).unwrap();
+    let reader = BufReader::new(file);
+    let mut lines = reader.lines();
+    let mut contents = String::new();
+
+    let mut removed = false;
+    while let Some(line) = lines.next() {
+        let line = line.unwrap();
+        if line.contains(format!("server {}", &ip).as_str()) {
+            removed = true;
+        } else {
+            contents.push_str(&line);
+            contents.push_str("\n");
+        }
+    }
+    let mut file = File::create(NGINX_CONF_PATH).unwrap();
+    file.write_all(contents.as_bytes()).unwrap();
+
+    let res = soft_reload_nginx().await;
+    if res.is_err() {
+        let output = Command::new("mv")
+            .arg(format!("{NGINX_CONF_BACKUP_PATH}"))
+            .arg(format!("{NGINX_CONF_PATH}"))
+            .output()
+            .expect("Failed to restore from backup file");
+
+        if !output.status.success() {
+            return Err(anyhow::anyhow!(
+                "Failed to restore from backup file: {}",
+                String::from_utf8(output.stderr).unwrap()
+            ));
+        }
+    }
+
+    let _ = filelock.unlock();
+
+    Ok(removed)
 }
